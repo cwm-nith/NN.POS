991db509a2393055c257feeeb12511a1ffc82454
codewithmecoder <yaty1031@gmail.com>
2025-02-13 20:59:10 +0700

    add StringLength

--- NN.POS.API/Infra/Tables/Roles/RoleTable.cs
+++ NN.POS.API/Infra/Tables/Roles/RoleTable.cs
@@ -1,17 +1,18 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace NN.POS.API.Infra.Tables.Roles;
 
 [Table("roles")]
 public class RoleTable : BaseTable
 {
-    [Column("name")]
+    [Column("name"), StringLength(150)]
     public string Name { get; set; } = string.Empty;
 
-    [Column("display_name")]
+    [Column("display_name"), StringLength(150)]
     public string? DisplayName { get; set; }
 
-    [Column("description")]
+    [Column("description"), StringLength(250)]
     public string? Description { get; set; }
 
     [Column("updated_at")]
